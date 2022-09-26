34abd35eb6d926e788bcc26a2649a282e00bba1b
Kaio Rosa <kaiosrosa@gmail.com>
2022-09-25 21:52:50 -0300

    add boilerplate http methods

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (request, response)=>{
+    return response.json({ message: "Getting your data"});
+});
+
+app.post("/", (request, response)=>{
+    return response.json({ message: "Saving your data"});
+});
+
 app.listen(3333,()=> console.log("Server is running!"));
