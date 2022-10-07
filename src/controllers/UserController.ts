2fe7ee4c9c37bd9e0baac753dc620cd3ba5d900c
Kaio Rosa <kaiosrosa@gmail.com>
2022-10-06 22:21:05 -0300

    add user CRUD operations

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -28,6 +28,24 @@ class UsersController {
         return response.status(201).json({ message: "User Created!", data: user });
     }
 
+    async find(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const userRepository = Manager.getRepository(User);
+
+        const foundUser = await userRepository.findOne(
+            {
+                where: { id }
+            }
+        );
+
+        if (!foundUser) {
+            return response.status(400).json({ error: "User does not exist!" })
+        }
+
+        return response.json({ message: "User Found!", data: foundUser });
+    }
+
     async update(request: Request, response: Response) {
         const { id } = request.params;
         const { name, email, password } = request.body;
@@ -52,6 +70,28 @@ class UsersController {
 
         return response.json({ message: "User Updated!", data: { name, email, password } });
     }
+
+
+    async delete(request: Request, response: Response) {
+        const { id } = request.params;
+        
+        const userRepository = Manager.getRepository(User);
+
+        const user = await userRepository.findOne(
+            {
+                where: { id }
+            }
+        );
+
+        if (!user) {
+            return response.status(400).json({ error: "Must be a valid User to delete!" })
+        }
+
+        await userRepository.delete(id);
+
+        return response.json({ message: "User Deleted!", data: user });
+    }
+
 }
 
 export { UsersController }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,6 +6,8 @@ const usersRoutes = Router();
 const userController = new UsersController();
 
 usersRoutes.post('/', userController.create);
-usersRoutes.post('/:id', userController.update);
+usersRoutes.put('/:id', userController.update);
+usersRoutes.get('/:id', userController.find);
+usersRoutes.delete('/:id', userController.delete);
 
 export { usersRoutes }
