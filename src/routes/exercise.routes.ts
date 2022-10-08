54aba2ce3b5ea10345329ca9445767e8c1ef20ab
Kaio Rosa <kaiosrosa@gmail.com>
2022-10-07 22:48:20 -0300

    add exercise CRUD routes

--- src/routes/exercise.routes.ts
+++ src/routes/exercise.routes.ts
@@ -6,5 +6,8 @@ const exerciseRoutes = Router();
 const exerciseController = new ExerciseController();
 
 exerciseRoutes.post('/', exerciseController.create);
+exerciseRoutes.put('/:id', exerciseController.update);
+exerciseRoutes.get('/:id', exerciseController.find);
+exerciseRoutes.delete('/:id', exerciseController.delete);
 
 export { exerciseRoutes }
